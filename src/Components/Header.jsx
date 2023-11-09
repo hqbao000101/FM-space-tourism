e80c23850330517b55912453d988f376009138ba
Huỳnh Quang Bảo <hbao592000@gmail.com>
2023-11-09 11:09:49 +0700

    header

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,17 +1,40 @@
-import React from 'react'
-import { NavLink } from 'react-router-dom'
+import React from "react";
+import { NavLink } from "react-router-dom";
+import Group2 from "../assets/icons/Group 2.png";
 
 const Header = () => {
   return (
-    <div>
+    <header>
+      <img src={Group2} alt="Group 2 Logo" />
+      <hr />
       <nav>
-        <NavLink to="/">Home</NavLink>
-        <NavLink to="/destination">Destination</NavLink>
-        <NavLink to="/crew">Crew</NavLink>
-        <NavLink to="/technology">Technology</NavLink>
+        <NavLink to="/">
+          <div className="nav_text active">
+            <span>00</span>
+            Home
+          </div>
+        </NavLink>
+        <NavLink to="/destination">
+          <div className="nav_text">
+            <span>01</span>
+            Destination
+          </div>
+        </NavLink>
+        <NavLink to="/crew">
+          <div className="nav_text">
+            <span>02</span>
+            Crew
+          </div>
+        </NavLink>
+        <NavLink to="/technology">
+          <div className="nav_text">
+            <span>03</span>
+            Technology
+          </div>
+        </NavLink>
       </nav>
-    </div>
-  )
-}
+    </header>
+  );
+};
 
-export default Header
+export default Header;
--- src/templates/UserTemplate.jsx
+++ src/templates/UserTemplate.jsx
@@ -1,12 +1,21 @@
 import React from "react";
 import Header from "../Components/Header";
 import { Outlet } from "react-router-dom";
+import BackgroundHomeDesktop from "../assets/home/background-home-desktop.jpg";
 
 const UserTemplate = () => {
   return (
-    <div>
-      <Header />
-      <Outlet />
+    <div
+      style={{
+        background: `url(${BackgroundHomeDesktop}), lightgray 0px 0px / 100% 100% no-repeat`,
+        mixBlendMode: "screen",
+        backgroundSize: "cover",
+      }}
+    >
+      <div className="h-screen py-10 mx-auto max-w-screen-2xl">
+        <Header />
+        <Outlet />
+      </div>
     </div>
   );
 };
